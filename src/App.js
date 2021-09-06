93ad6d869dc15f70d5c8d74a2554a7a84c852ea3
faizan76 <mfaizan76@gmail.com>
2021-09-06 14:17:55 +0500

    Switch to Routes in App(c) load first Matched

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HomePage from './pages/HomePage';
-import { BrowserRouter as Router, Route, } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import NavBar from './NavBar';
@@ -12,11 +12,13 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
-          <Route path="/" component={HomePage} exact />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/articles-list" component={ArticlesListPage} />
-          <Route path="/article/:name" component={ArticlePage} />
-          <Route component={NotFoundPage} />
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesListPage} />
+            <Route path="/article/:name" component={ArticlePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
