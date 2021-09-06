a16bda0488b127cc40a7ae4cf025262c4ea364db
faizan76 <mfaizan76@gmail.com>
2021-09-06 14:04:44 +0500

    Route ArticlePage(c) in App(c)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, } from 'react-router-dom';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import NavBar from './NavBar';
+import ArticlePage from './pages/ArticlePage';
 function App() {
   return (
     <Router>
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
           <Route path="/articles-list" component={ArticlesListPage} />
+          <Route path="/article/:name" component={ArticlePage} />
         </div>
       </div>
     </Router>
