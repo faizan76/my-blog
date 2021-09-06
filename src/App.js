f08db9f9214c4ea39f033eedd73bba5773a54d26
faizan76 <mfaizan76@gmail.com>
2021-09-06 12:50:02 +0500

    Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <HomePage />
+      </div>
+    </Router>
   );
 }
 
