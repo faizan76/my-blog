78a90f6019d94517d497f0ef867da59f9c2c946d
faizan76 <mfaizan76@gmail.com>
2021-09-06 13:46:30 +0500

    Navbar(c) in App(c)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Route, } from 'react-router-dom';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
+import NavBar from './NavBar';
 function App() {
   return (
     <Router>
+      <NavBar />
       <div className="App">
         <Route path="/" component={HomePage} exact />
         <Route path="/about" component={AboutPage} />
