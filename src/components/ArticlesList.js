068d6c4aa8bc6872b6d7b63e40ee8d4059d95fac
faizan76 <mfaizan76@gmail.com>
2021-09-06 13:26:32 +0500

    article-content>ArticlesListPage>ArticlesList

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ArticlesList = () => (
+const ArticlesList = ({ articles }) => (
     <>
     </>
 );
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ArticlesList from '../components/ArticlesList';
+import articleContent from './article-content';
 const ArticlesListPage = () => (
     <>
         <h1>Articles</h1>
-        <ArticlesList />
+        <ArticlesList articles={articleContent} />
     </>
 );
 
