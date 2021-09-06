3a8e62fbc9283ed9f44ab962fc2f6691daa44794
faizan76 <mfaizan76@gmail.com>
2021-09-06 13:05:07 +0500

    AboutPage(c) in App(c)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Route, } from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
 function App() {
   return (
     <Router>
       <div className="App">
         <Route path="/" component={HomePage} exact />
+        <Route path="/" component={AboutPage} />
       </div>
     </Router>
   );
